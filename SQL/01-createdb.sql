327c23f3266e4bbe165b44870fca113ef6151370
Koh Hui Soon <huisoonkoh@gmail.com>
2022-10-31 00:26:41 +0800

    Scripts for database

---
+++ SQL/01-createdb.sql
@@ -0,0 +1,14 @@
+DROP SCHEMA IF EXISTS `todo_db`;
+
+CREATE SCHEMA `todo_db`;
+
+USE `todo_db`;
+
+DROP TABLE IF EXISTS `todo`;
+
+CREATE TABLE `todo` (
+    `id` varchar(36) NOT NULL,
+    `user_id` varchar(36) NOT NULL,
+    `label` varchar(80) NOT NULL,
+    `done` tinyint(1) NOT NULL
+)
